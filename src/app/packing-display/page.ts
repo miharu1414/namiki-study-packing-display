07729ce64dd6a40a36c98835b3b57fb4b8cc0a82
MiharuNamiki <79990189+miharu1414@users.noreply.github.com>
2024-12-02 22:12:17 +0900

    build: デプロイお試し

---
+++ src/app/packing-display/page.ts
@@ -0,0 +1,3 @@
+import { PackingDisplay } from '@/lib/pages/packing-display';
+
+export default PackingDisplay;
---
+++ src/lib/pages/packing-display/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { Flex } from '@chakra-ui/react';
+
+export const PackingDisplay = () => {
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="70vh"
+      gap={4}
+      mb={8}
+      w="full"
+    ></Flex>
+  );
+};
