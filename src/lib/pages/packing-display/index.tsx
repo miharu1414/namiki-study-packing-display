908caf869d5d61e62a97bf358bad3433855e3c20
MiharuNamiki <79990189+miharu1414@users.noreply.github.com>
2024-12-30 21:34:53 +0900

    feat(pages): プロトタイプのファーストリリース

--- src/lib/pages/packing-display/index.tsx
+++ src/lib/pages/packing-display/index.tsx
@@ -1,18 +1,5 @@
 'use client';
-import { Flex } from '@chakra-ui/react';
-import { CanvasComponent } from './components/CanvasComponent';
+import { PresentationPacking } from './components/Presentation';
 export const PackingDisplay = () => {
-  return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      minHeight="70vh"
-      gap={4}
-      mb={8}
-      w="full"
-    >
-      <CanvasComponent />
-    </Flex>
-  );
+  return <PresentationPacking />;
 };
